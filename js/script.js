ba193bdc80f82862c13636add8d80a9d249099cc
ilhamjuliano <ilhamjulianto98@gmail.com>
2023-08-05 22:18:05 +0700

    Create Parallax Effect

--- js/script.js
+++ js/script.js
@@ -15,3 +15,51 @@ $('.page-scroll').on('click', function(e){
 	e.preventDefault();
 	
 });
+
+
+// Parallax
+
+// About
+$(window).on('load', function() {
+	$('.pKiri').addClass('pTampil');
+	$('.pKanan').addClass('pTampil');
+});
+
+$(window).scroll(function() {
+	var wScroll = $(this).scrollTop();
+
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+wScroll/4+'%)'
+
+	});
+});
+
+$(window).scroll(function() {
+	var wScroll = $(this).scrollTop();
+
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+wScroll/8+'%)'
+
+	});
+});
+
+$(window).scroll(function() {
+	var wScroll = $(this).scrollTop();
+
+	$('.jumbotron p').css({
+		'transform' : 'translate(0px, '+wScroll/100+'%)'
+
+	});
+
+
+	// Portfolio
+	if (wScroll > $(' .portfolio').offset().top - 250) {
+		$('.portfolio .thumbnail').each(function(i){
+			setTimeout(function(){
+				$('.portfolio .thumbnail').eq(i).addClass('tampil');
+			}, 300 * (i+1));
+		});
+		//
+	}
+
+});
